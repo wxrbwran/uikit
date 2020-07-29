155c156d341512528b9d5150a59d2bcd52edd63b
wuxiaoran <wuxiaoran@xinzhili.cn>
2020-07-29 18:33:46 +0800

    save

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -4,6 +4,7 @@ import {
   fireEvent,
   RenderResult,
   cleanup,
+  wait,
 } from "@testing-library/react";
 import Menu from "./menu";
 import MenuItem from "./menuItem";
@@ -29,7 +30,7 @@ const generateMenu = (props) => {
       <MenuItem disabled>disabled</MenuItem>
       <SubMenu title="sub">
         <MenuItem>s1</MenuItem>
-        <MenuItem>wws2</MenuItem>
+        <MenuItem>s2</MenuItem>
       </SubMenu>
     </Menu>
   );
@@ -78,8 +79,16 @@ describe("测试Menu 和 MenuItem 组件", () => {
     expect(disabledElement).not.toHaveClass("is-active");
     expect(testProps.onSelect).not.toHaveBeenCalledWith("2");
   });
-  it("在SubMenu上hover时，显示dropdown menu", () => {
+  it("在SubMenu上hover时，显示dropdown menu", async () => {
     expect(wrapper.queryByText("s1")).not.toBeVisible();
+    const dropdownElement = wrapper.getByText("sub");
+    fireEvent.mouseEnter(dropdownElement);
+    await wait(() => {
+      expect(wrapper.queryByText("s1")).toBeVisible();
+    });
+    fireEvent.click(wrapper.getByText("s1"));
+    expect(testProps.onSelect).toHaveBeenCalledWith("3-0");
+    // expect(wrapper.queryByText("s1")).not.toBeVisible();
   });
 
   it("传入mode=’vertical‘, 渲染竖排menu", () => {
