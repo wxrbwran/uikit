9ba38d3d9f327c205388f2ff3a37c9a2cf414605
wuxiaoran <wuxiaoran@xinzhili.cn>
2020-08-03 18:31:55 +0800

    icon

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -1,9 +1,9 @@
-import React  from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { Upload } from './upload'
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { Upload } from "./upload";
 //import Button from '../Button/button'
-import Icon from '../Icon/icon'
+import Icon from "../Icon/icon";
 // const defaultFileList: UploadFile[] = [
 //   { uid: '123', size: 1234, name: 'hello.md', status: 'uploading', percent: 30 },
 //   { uid: '122', size: 1234, name: 'xyz.md', status: 'success', percent: 30 },
@@ -20,22 +20,22 @@ import Icon from '../Icon/icon'
 //   const newFile = new File([file], 'new_name.docx', {type: file.type})
 //   return Promise.resolve(newFile)
 // }
-const SimpleUpload = () => {
+export const SimpleUpload = () => {
   return (
     <Upload
       action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-      onChange={action('changed')}
-      onRemove={action('removed')}
+      onChange={action("changed")}
+      onRemove={action("removed")}
+      onProgress={action("progressed")}
       name="fileName"
       multiple
       drag
     >
       <Icon icon="upload" size="5x" theme="secondary" />
-      <br/>
+      <br />
       <p>Drag file over to upload</p>
     </Upload>
-  )
-}
+  );
+};
 
-storiesOf('Upload component', module)
-  .add('Upload', SimpleUpload)
+storiesOf("Upload component", module).add("Upload", SimpleUpload);
