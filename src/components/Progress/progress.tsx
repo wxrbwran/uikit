a39e7a6529f9a7804ec4eff629cb29077c347dcf
wuxiaoran <wuxiaoran@xinzhili.cn>
2020-08-03 18:26:23 +0800

    upload

--- src/components/Progress/progress.tsx
+++ src/components/Progress/progress.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react'
-import { ThemeProps } from '../Icon/icon'
+import React, { FC } from "react";
+import { ThemeProps } from "../Icon/icon";
 export interface ProgressProps {
   percent: number;
   strokeHeight?: number;
@@ -8,31 +8,28 @@ export interface ProgressProps {
   theme?: ThemeProps;
 }
 
-const Progress: FC<ProgressProps> = (props) => {
-  const {
-    percent,
-    strokeHeight,
-    showText,
-    styles,
-    theme,
-  } = props
+const Progress: FC<ProgressProps> = props => {
+  const { percent, strokeHeight, showText, styles, theme } = props;
   return (
-    <div className="viking-progress-bar" style={styles}>
-      <div className="viking-progress-bar-outer" style={{ height: `${strokeHeight}px`}}>
-        <div 
-          className={`viking-progress-bar-inner color-${theme}`}
-          style={{width: `${percent}%`}}
+    <div className="xr-progress-bar" style={styles}>
+      <div
+        className="xr-progress-bar-outer"
+        style={{ height: `${strokeHeight}px` }}
+      >
+        <div
+          className={`xr-progress-bar-inner color-${theme}`}
+          style={{ width: `${percent}%` }}
         >
           {showText && <span className="inner-text">{`${percent}%`}</span>}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 Progress.defaultProps = {
   strokeHeight: 15,
   showText: true,
-  theme: "primary",
-}
+  theme: "primary"
+};
 export default Progress;
